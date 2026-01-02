f38ea98e66efea88688e4fa000cc176a171cd35d
zythdotdev <15996340+zythdotdev@users.noreply.github.com>
2026-01-02 19:04:31 +0000

    Built and published by Moonwave

---
+++ assets/js/9e73874d.24653e28.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[795],{40239:e=>{e.exports=JSON.parse('[{"value":"Types","id":"types","level":2},{"value":"Self<Args,Return>","id":"Self<Args,Return>","level":3},{"value":"Properties","id":"properties","level":2},{"value":"Functions","id":"functions","level":2},{"value":".Destroy","id":"Destroy","level":3}]')}}]);
